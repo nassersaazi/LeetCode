73d78e31d7506d2d069094428a7a513b33f93e32
nassersaazi <nassersnoop@gmail.com>
2023-01-27 21:19:38 +0300

    Day 1 daily interview practice

--- python/alreadySolved/202_happy_number.py
+++ python/alreadySolved/202_happy_number.py
@@ -9,10 +9,17 @@ class Solution:
         while n != 1:
             sum = 0
             while n:
+                print("n before divmod " + str(n))
                 n, digit = divmod(n, 10)
                 sum += digit * digit
+                print("n after divmod " + str(n))
+
+                print("visited set " + str(visited))
+                print("sum is" + str(sum))
+                print("\n")
             if sum in visited:
                 return False
+
             n = sum
             visited.add(n)
         return True
@@ -24,8 +31,20 @@ solution = Solution()
 n = 19
 assert solution.isHappy(n)
 
-n = 2
-assert not solution.isHappy(n)
+#n = 2
+#assert not solution.isHappy(n)
 
-n = 1
-assert solution.isHappy(n)
+#n = 1
+#assert solution.isHappy(n)
+
+
+# Alternative solution
+## class Solution(object):
+#    def isHappy(self, n):
+#        hset = set()
+#        while n != 1:
+#            if n in hset: return False
+#            hset.add(n)
+#            n = sum([int(i) ** 2 for i in str(n)])
+#        else:
+#            return True
