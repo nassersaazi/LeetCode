613b976ea4cc72cbca92513381a25a201850c39a
nassersaazi <nassersnoop@gmail.com>
2023-01-29 20:24:28 +0300

    Better solution to Happy number problem, plus some notes on python in general

--- python/alreadySolved/202_happy_number.py
+++ python/alreadySolved/202_happy_number.py
@@ -3,27 +3,14 @@
 
 
 class Solution:
-    def isHappy(self, n: int) -> bool:  # noqa: N802
-        visited: set[int] = set()
-        visited.add(n)
+    def isHappy(self, n: int) -> bool:
+        hset = set()
         while n != 1:
-            sum = 0
-            while n:
-                print("n before divmod " + str(n))
-                n, digit = divmod(n, 10)
-                sum += digit * digit
-                print("n after divmod " + str(n))
-
-                print("visited set " + str(visited))
-                print("sum is" + str(sum))
-                print("\n")
-            if sum in visited:
-                return False
-
-            n = sum
-            visited.add(n)
-        return True
-
+            if n in hset: return False
+            hset.add(n)
+            n = sum([int(i) ** 2 for i in str(n)])
+        else:
+            return True
 
 solution = Solution()
 
@@ -31,20 +18,10 @@ solution = Solution()
 n = 19
 assert solution.isHappy(n)
 
-#n = 2
-#assert not solution.isHappy(n)
+n = 2
+assert not solution.isHappy(n)
 
-#n = 1
-#assert solution.isHappy(n)
+n = 1
+assert solution.isHappy(n)
 
 
-# Alternative solution
-## class Solution(object):
-#    def isHappy(self, n):
-#        hset = set()
-#        while n != 1:
-#            if n in hset: return False
-#            hset.add(n)
-#            n = sum([int(i) ** 2 for i in str(n)])
-#        else:
-#            return True
